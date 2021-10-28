7f69127528a03fbb2ff853d4509fb5afafbbd78a
Rajwinder Bhatoe <rajwinder.bhatoe@gmail.com>
2021-10-28 14:08:24 +0100

    Basic structure formatted

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,15 @@
-import { greet } from "./utils/greet";
+import AppHeader from "./components/AppHeader";
+import AppFooter from "./components/AppFooter";
+import Episodes from "./components/Episodes";
 
 function App(): JSX.Element {
-  return <h1>{greet("World")}</h1>;
+  return (
+    <>
+      <AppHeader />
+      <Episodes />
+      <AppFooter />
+    </>
+  );
 }
 
 export default App;
---
+++ src/components/AppFooter.tsx
@@ -0,0 +1,7 @@
+export default function AppFooter(): JSX.Element {
+  return (
+    <footer>
+      <h2>footer</h2>
+    </footer>
+  );
+}
