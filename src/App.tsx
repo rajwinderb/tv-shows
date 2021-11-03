854b010923fcc0bebc5305c8b6ba96bdde7ea648
Rajwinder Bhatoe <rajwinder.bhatoe@gmail.com>
2021-11-03 16:23:09 +0000

    Uses API to get the episodes rather than the JSON file

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,45 @@
-import { useState } from "react";
-import episodes from "./episodes.json";
+import { useState, useEffect } from "react";
 import "./App.css";
 import AppHeader from "./components/AppHeader";
 import AppFooter from "./components/AppFooter";
 import Episodes from "./components/Episodes";
 import NavBar from "./components/NavBar";
 
+interface IEpisode {
+  id: number;
+  url: string;
+  name: string;
+  season: number;
+  number: number;
+  type: string;
+  airdate: string;
+  airtime: string;
+  airstamp: string;
+  runtime: number;
+  image: {
+    medium: string;
+    original: string;
+  };
+  summary: string;
+  _links: { self: { href: string } };
+}
+
 function App(): JSX.Element {
-  const episodelist = episodes;
+  const [episodelist, setEpisodeList] = useState<IEpisode[]>([]);
   const [searchText, setSearchText] = useState<string>("");
   const [allEpisodes, setAllEpisodes] = useState<number>(0);
   const [numResults, setNumResults] = useState<number>(0);
   const [episodePicker, setEpisodePicker] = useState<string>("");
+
+  useEffect(() => {
+    const fetchEpisodes = () => {
+      fetch("https://api.tvmaze.com/shows/82/episodes")
+        .then((response) => response.json())
+        .then((jsonBody: IEpisode[]) => setEpisodeList(jsonBody));
+    };
+
+    fetchEpisodes();
+  }, []);
 
   return (
     <>
